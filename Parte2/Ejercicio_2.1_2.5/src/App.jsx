9736501f83c5a4038ca84b7a50d6338eb40b86b1
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-01-04 17:52:40 -0600

    2.1: Información del curso paso6

---
+++ Parte2/Ejercicio_2.1_2.5/src/App.jsx
@@ -0,0 +1,63 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.sumOfExercises}</p>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0].name} exercises={props.part[0].exercises} />
+      <Part part={props.part[1].name} exercises={props.part[1].exercises} />
+      <Part part={props.part[2].name} exercises={props.part[2].exercises} />
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  console.log(course)
+  return(
+    <div>
+      <Header course={course.name} />
+      <Content part={course.parts} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
+
+export default App
