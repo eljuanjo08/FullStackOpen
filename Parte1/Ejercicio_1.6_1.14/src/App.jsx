80a667dc46ff370a291150efcdf5b4c8afb012ac
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-01-03 19:04:13 -0600

    1.8: unicafe, paso 3

--- Parte1/Ejercicio_1.6_1.14/src/App.jsx
+++ Parte1/Ejercicio_1.6_1.14/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Display = ({text ,value, percentage = false}) => {
   if (percentage == true) {
@@ -12,32 +12,20 @@ const Display = ({text ,value, percentage = false}) => {
   }
 }
 
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
-// Declaramos el componente del total de comentarios
-
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const setToGood = () => setGood(good + 1);
-  const setToNeutral = () => setNeutral(neutral + 1);
-  const setToBad = () => setBad(bad + 1);
+// Declaramos el componente de las estadisticas
+const Statistics = ({good, bad, neutral}) => {
   const total = good + neutral + bad;
+  // Funcion para rendondear a centesimas
   const average = Math.round(((good * 1) + (neutral * 0) + (bad * -1) / 3) * 100) / 1000;
-  const positive = Math.round((good / total) * 10000) / 100;
-  
-  return (
-    <div>
-      <h1>give feedback</h1>
-      <Button handleClick={setToGood} text='Good' />
-      <Button handleClick={setToNeutral} text='Neutral' />
-      <Button handleClick={setToBad} text='Bad' />
+  let positive = 0;
+  // Utilizamos el operador ternario para cuando no haya valores para good, bad o neutral para poner valor 0 a positive
+  good !== 0 && bad !== 0 && neutral !== 0 ? positive = Math.round((good / total) * 10000) / 100 : positive = 0;
+  return(
+    <>
       <h2>Statistics</h2>
       <Display text='good' value={good} />
       <Display text='neutral' value={neutral} />
@@ -45,8 +33,29 @@ const App = () => {
       <Display text='Total' value={total} />
       <Display text='Average' value={average} />
       <Display text='Positive' value={positive} percentage={true} />
-    </div>
+    </>
   )
 }
 
-export default App
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const setToGood = () => setGood(good + 1);
+  const setToNeutral = () => setNeutral(neutral + 1);
+  const setToBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={setToGood} text="Good" />
+      <Button handleClick={setToNeutral} text="Neutral" />
+      <Button handleClick={setToBad} text="Bad" />
+      <Statistics good={good} bad={bad} neutral={neutral}  />
+    </div>
+  );
+};
+
+export default App;
