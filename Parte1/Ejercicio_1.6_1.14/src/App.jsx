7ed46732084be9acda3e94aaf1c29162e4dba7a4
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-01-03 18:52:01 -0600

    1.7: unicafe, paso 2

--- Parte1/Ejercicio_1.6_1.14/src/App.jsx
+++ Parte1/Ejercicio_1.6_1.14/src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react';
 
-const Display = ({text ,value}) => <div>{text}: {value}</div>
+const Display = ({text ,value, percentage = false}) => {
+  if (percentage == true) {
+    return(
+      <div>{text}: {value}%</div>
+      )
+  } else {
+    return(
+      <div>{text}: {value}</div>
+      )
+  }
+}
 
 const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
+
+// Declaramos el componente del total de comentarios
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -16,7 +28,10 @@ const App = () => {
   const setToGood = () => setGood(good + 1);
   const setToNeutral = () => setNeutral(neutral + 1);
   const setToBad = () => setBad(bad + 1);
-
+  const total = good + neutral + bad;
+  const average = Math.round(((good * 1) + (neutral * 0) + (bad * -1) / 3) * 100) / 1000;
+  const positive = Math.round((good / total) * 10000) / 100;
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -27,6 +42,9 @@ const App = () => {
       <Display text='good' value={good} />
       <Display text='neutral' value={neutral} />
       <Display text='bad' value={bad} />
+      <Display text='Total' value={total} />
+      <Display text='Average' value={average} />
+      <Display text='Positive' value={positive} percentage={true} />
     </div>
   )
 }
