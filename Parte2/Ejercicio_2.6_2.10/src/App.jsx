9c2113becf2a407fe755c80ad18f73d7d31355b2
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-01-05 15:11:27 -0600

    2.8: La guía telefónica Paso 3

--- Parte2/Ejercicio_2.6_2.10/src/App.jsx
+++ Parte2/Ejercicio_2.6_2.10/src/App.jsx
@@ -1,42 +1,51 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "123-4567-890" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const handleChangePersons = (event) => {
+    setNewName(event.target.value);
+  };
 
   const handleChangeNumbers = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const addPerson = (event) => {
     event.preventDefault();
     const newPerson = {
-      name: newName
-    }
+      name: newName,
+      number: newNumber
+    };
 
-    const findPerson = persons.find(person => person.name === newName);
+    const findPerson = persons.find((person) => person.name === newName);
     if (findPerson === undefined) {
       setPersons(persons.concat(newPerson));
       setNewName("");
     } else {
-      alert(`${newName} is alredy added to phoneook`)
+      alert(`${newName} is alredy added to phoneook`);
     }
-  }
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input onChange={handleChangeNumbers} value={newName} />
+          name: <input onChange={handleChangePersons} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleChangeNumbers} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => (
-        <div key={person.name}>{person.name}</div>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name} {person.number}</div>
       ))}
     </div>
   );
