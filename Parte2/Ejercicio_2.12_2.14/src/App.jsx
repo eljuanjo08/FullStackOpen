d6ac30ef5cdf8c8abacac2d1626613c8835049eb
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-01-09 16:22:26 -0600

    2.13*: Datos para países, paso 2

--- Parte2/Ejercicio_2.12_2.14/src/App.jsx
+++ Parte2/Ejercicio_2.12_2.14/src/App.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import CountriesDisplay from './components/CountriesDisplay'
 import { useEffect, useState } from "react";
 
 const App = () => {
@@ -23,72 +24,19 @@ const App = () => {
       -1
   );
 
-  if (searchCountry.length === 1) {
-    return (
+  return (
+    <>
       <div>
-        <div>
-          find countries{" "}
-          <input
-            type="text"
-            onChange={handleChange}
-            placeholder="search country"
-          />
-          {searchCountry.map((country) => (
-            <>
-              <h1 key={country.name.common}>{country.name.common}</h1>
-              <div key={country.name.common}>Capital: {country.capital}</div>
-              <div key={country.name.common}>
-                Population: {country.population}
-              </div>
-              <img
-                key={country.name.common}
-                src={country.flags.png}
-                width="100"
-              />
-              <h2 key={country.name}>Languages</h2>
-              <ul key={country.name}>
-                {Object.values(country.languages).map((language) => (
-                  <li key={language}>{language}</li>
-                ))}
-              </ul>
-            </>
-          ))}
-        </div>
+        find countries{" "}
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="search country"
+        />
       </div>
-    );
-  } else if (searchCountry.length <= 10 && searchCountry.length != 0) {
-    return (
-      <div>
-        <div>
-          find countries{" "}
-          <input
-            type="text"
-            onChange={handleChange}
-            placeholder="search country"
-          />
-          {searchCountry.map((country) => (
-            <>
-              <div key={country.name.common}>{country.name.common}</div>
-            </>
-          ))}
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <div>
-          find countries{" "}
-          <input
-            type="text"
-            onChange={handleChange}
-            placeholder="search country"
-          />
-          <div>Too many matches, specify another filter</div>
-        </div>
-      </div>
-    );
-  }
+      <CountriesDisplay searchCountry={searchCountry} />
+    </>
+  );
 };
 
 export default App;
